c48c2ae85b7b9d4b1377df44f7f99e04fca69198
Yebox <yebox@Yeboxs-MacBook-Pro.local>
2023-07-28 20:36:01 +0100

    implemented api and storage

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,26 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "mycontactapp74c4b5bf": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "contactbucket": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    },
+    "api": {
+        "mycontactapi": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,42 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateContact = /* GraphQL */ `
+  subscription OnCreateContact {
+    onCreateContact {
+      id
+      name
+      email
+      cell
+      profilePicPath
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateContact = /* GraphQL */ `
+  subscription OnUpdateContact {
+    onUpdateContact {
+      id
+      name
+      email
+      cell
+      profilePicPath
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteContact = /* GraphQL */ `
+  subscription OnDeleteContact {
+    onDeleteContact {
+      id
+      name
+      email
+      cell
+      profilePicPath
+      createdAt
+      updatedAt
+    }
+  }
+`;
